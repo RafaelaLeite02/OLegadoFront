15f8b4e27d000e1fe00ceba3b41d42d571c47396
Rafaela Nascimento Leite <rafaelaleite2017@iclod.com>
2025-09-23 10:29:01 -0300

    Footer completo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,14 @@
 <template>
-    <section class="w-auto  bg-gradient-to-b from-stone-950 to-gray-900 flex flex-col p-2 ">
-        <div>
-            <hr class=" border-0 bg-gradient-to-r from-stone-950 via-red-800 to-stone-950" >
+    <hr class="h-1 border-0 bg-gradient-to-r from-stone-950 via-red-700 to-stone-950">
+    <section
+        class="w-auto h-50 bg-gradient-to-b from-stone-950 to-gray-900 flex flex-col justify-center items-center p-2 ">
+        <div class="flex flex-row text-neutral-50 items-center p-5 text-[20px] ">
+            <img src="/public/img/coracaoicon.png" alt="coração" class="w-8 h-8 m-3" />
+            <p> Deselvolvido com a paixão pelos imortais </p>
+            <img src="/public/img/vampiroicon.png" alt="vampiro" class="w-12 h-12 m-3" />
         </div>
-        <div class="text-neutral-50 p-2 ">
-            &copy; 2025 O Legado Of Blood
+        <div class=" text-neutral-500 ">
+            &copy; 2025 O Legado Of Blood. Todos os direitos reservados aos filhos da noite.
         </div>
     </section>
 </template>
